a69af0ab1997dbc7d9ee9825631342a41b4b1f88
MisterConscio <saulo.costam@gmail.com>
2023-07-20 13:54:04 -0300

    Making post images work and handle mouse click

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,20 +1,44 @@
+import { useState } from "react";
 import "../styles/Post.css";
 
 type Props = {
   name?: string;
-  image?: string;
+  image?: number;
+  ext: string;
   number: number;
   date: string;
   comment: string;
+  tn_w: number;
+  tn_h: number;
+  width: number;
+  height: number;
 };
 
-const Post = ({ name = "Anonymous", image, number, date, comment }: Props) => {
+const Post = ({
+  name = "Anonymous",
+  image,
+  ext,
+  number,
+  date,
+  comment,
+  tn_w,
+  tn_h,
+  width,
+  height,
+}: Props) => {
+  const [fullImg, setFullImg] = useState(false);
+
+  const handelImgClick = () => {
+    setFullImg(!fullImg);
+  };
+
   return (
-    <div className="post" key={number}>
+    <div className="post" key={number} id={number.toString()}>
       <div className="post-info">
         <span className="post-info-name">{name}</span>
         <span className="post-info-date">{date}</span>
         <span className="post-info-number">No. {number.toString()}</span>
+        {/*
         <div className="post-info-backlink">
           <span>
             <a href="#">&gt;&gt;94731503</a>
@@ -23,16 +47,34 @@ const Post = ({ name = "Anonymous", image, number, date, comment }: Props) => {
             <a href="#">&gt;&gt;94731503</a>
           </span>
         </div>
+        */}
       </div>
-      <div className="post-message">
+      <div className={`post-message ${fullImg && "post-message-full"}`}>
         {image && (
-          <div className="post-message-img">
-            <img src={image} alt="thumb" />
-          </div>
+          <a onClick={() => handelImgClick()} className="post-message-img">
+            {fullImg ? (
+              <img
+                src={`https://placehold.it/${width}x${height}`}
+                width={width}
+                height={height}
+                alt="thumb"
+                loading="lazy"
+              />
+            ) : (
+              <img
+                src={`https://placehold.it/${tn_w}x${tn_h}`}
+                width={tn_w}
+                height={tn_h}
+                alt="thumb"
+                loading="lazy"
+              />
+            )}
+          </a>
         )}
-        <div className="post-message-reply">
-          <div dangerouslySetInnerHTML={{ __html: comment }} />
-        </div>
+        <div
+          className="post-message-reply"
+          dangerouslySetInnerHTML={{ __html: comment }}
+        />
       </div>
     </div>
   );
--- src/pages/ThreadPage.tsx
+++ src/pages/ThreadPage.tsx
@@ -13,6 +13,12 @@ type Thread = {
   now: string;
   name: string;
   com: string;
+  tim: number;
+  ext: string;
+  tn_w: number;
+  tn_h: number;
+  w: number;
+  h: number;
 };
 
 const ThreadPage = () => {
@@ -34,8 +40,8 @@ const ThreadPage = () => {
       const threadData: Promise<any> = await response.json();
       const postData: Thread[] = [];
       for (const post of threadData.posts) {
-        const { no, now, name, com } = post;
-        postData.push({ no, now, name, com });
+        const { no, now, name, com, tim, ext, tn_w, tn_h, w, h } = post;
+        postData.push({ no, now, name, com, tim, ext, tn_w, tn_h, w, h });
       }
       setThread(postData);
     } catch (error) {
@@ -54,6 +60,12 @@ const ThreadPage = () => {
             name={post.name}
             date={post.now}
             comment={post.com}
+            image={post.tim}
+            ext={post.ext}
+            tn_w={post.tn_w}
+            tn_h={post.tn_h}
+            width={post.w}
+            height={post.h}
           />
         ))}
       </main>
